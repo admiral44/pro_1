01b095a1d2a427fb597b434571eaba8a40e365b9
Admirl44 <36560341+admiral44@users.noreply.github.com>
2021-12-30 17:41:16 +0530

    master

--- ch1/ch1.js
+++ ch1/ch1.js
@@ -1,5 +1,21 @@
 const path = require('path');
 
+console.log("<---- Start ---->");
 console.log(path.basename('c:/pro_1/ch1/ch1.js'));
 console.log(path.basename(__filename));
 console.log(path.basename(__filename,'.js'));
+console.log("<---- End ---->");
+console.log(" ");
+
+// Directery pathe is display as per line of code.
+console.log("<---- Start ---->");
+console.log(path.dirname('c:/shubham/nagane/ch1/node.js'));
+console.log(path.dirname(__filename));
+console.log("<---- End ---->");
+console.log(" ");
+
+// Extanstion Name.
+console.log("<---- Start ---->");
+console.log(path.extname(__filename));
+console.log("<---- End ---->");
+console.log(" ");
